ac408b6b5e24932d2e4c7df9312cdb01454309eb
unknown <asko2212@hotmail.com>
2021-04-09 10:23:35 +0200

    sidebar started

--- pages/chat.js
+++
@@ -1,9 +0,0 @@
-const chat = () => {
-    return (
-        <div>
-            <h1>Ovo je CHAT stranica</h1>
-        </div>
-    )
-}
-
-export default chat
--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,15 @@
 import Head from 'next/head';
-import styles from '../styles/Home.module.css';
+import Sidebar from '../components/Sidebar';
 
 export default function Home() {
     return (
-        <div className={styles.container}>
+        <div>
             <Head>
-                <title>Create Next App</title>
+                <title>WhatsUp</title>
                 <link rel='icon' href='/favicon.ico' />
             </Head>
 
-            <main>
-                <h1>WhatsUP clone</h1>
-            </main>
+            <Sidebar />
         </div>
     );
 }
